e6b03c5c1fc27c953686611fbf27ffa22a6b8b44
Sam <samyankun@gmail.com>
2018-08-13 14:24:24 +0800

    support mongodb client

--- nodejs-assets/nodejs-project/main.js
+++ nodejs-assets/nodejs-project/main.js
@@ -1,8 +1,10 @@
 var http = require('http');
 var Eos = require('oasisjs')
 var rn_bridge = require('rn-bridge');
+var MongoClient = require('mongodb').MongoClient;
 
 let msg_number=0;
+var mongodburl = 'mongodb://124.127.156.41:27017/'
 
 var listVersionsServer = http.createServer( (request, response) => {
   response.end('Versions: ' + JSON.stringify(process.versions));
@@ -12,7 +14,8 @@ let listVersionsHTTPServer = listVersionsServer.listen(3001);
 var config = {
   chainId: "c40a90d6bcb4b9b2c2d4c0916ee97a29af42a420372af44fa4f538ddef9e6b83", // 32 byte (64 char) hex string
   keyProvider: ['5KZ2ytRsGMxRAycpFqFnkRF8mNfZTomQKnaXzh1FtbRPgbaTAF3','5Hz2G2L9p3k7YhkqGJaioNJQnYKjtZKKS2y3wRLDobCQAtXg5oA','5KRwwqFRdZ1v5UNcXPk72Mq3t4ucs7kMmqKx9HLpUnnk74iKWen'], // WIF string or array of keys..
-  httpEndpoint: 'http://192.168.0.109:8888',
+  httpEndpoint: 'http://124.127.156.41:8888',
+  // httpEndpoint: 'http://192.168.1.212:8888',
   expireInSeconds: 60,
   broadcast: true,
   verbose: false, // API activity
@@ -37,7 +40,7 @@ rn_bridge.channel.on('message', (msg) => {
       transferPara = {
         "from": 'eosio.token',
         "to": 'eosio',
-        "quantity": '134.0000 EOS',
+        "quantity": '100.0000 EOS',
         "memo": 'by react native'
       };
       eos.transfer(transferPara,(error,result)=>{
@@ -64,6 +67,22 @@ rn_bridge.channel.on('message', (msg) => {
     case 'resume':
       if(!listVersionsHTTPServer.listening)
         listVersionsHTTPServer = listVersionsServer.listen(3001);
+      break;
+    case 'blocks':
+      MongoClient.connect(mongodburl, function(err, db) {
+        if (err) {
+          throw err
+        }
+        var dbo = db.db('EOS')
+        dbo.collection('accounts').find({}).toArray((err, result) => {
+          if(err) {
+            throw err
+          }
+          rn_bridge.channel.send(result[0].name)
+          db.close()
+
+        })
+      })
       break;
     default:
       break;
